cc03fe6529a0e74bd1ced06d6249bc3eef04dbf3
godmajid <alfinmajid19@gmail.com>
2025-12-23 23:04:36 +0700

    indentasi fix

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,13 +17,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_metric("accuracy", acc)
+mlflow.log_param("n_estimators", 100)
+mlflow.log_metric("accuracy", acc)
 
-    mlflow.sklearn.log_model(model, "model")
+mlflow.sklearn.log_model(model, "model")
