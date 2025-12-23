03d399e97a8bc6bf36371fb6680d46a62d4b0088
godmajid <alfinmajid19@gmail.com>
2025-12-23 22:56:44 +0700

    run mlflowww

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -17,7 +17,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-with mlflow.start_run():
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
