84b3901ee24b52e64e55f95c5cbcf2599b7ca903
godmajid <alfinmajid19@gmail.com>
2025-12-23 21:53:28 +0700

    Initial push

---
+++ MLProject/modelling.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.set_experiment("CI-Training")
+
+df = pd.read_csv("water_potability_preprocessing.csv")
+
+X = df.drop("Potability", axis=1)
+y = df["Potability"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(model, "model")
